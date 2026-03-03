6870e804b10e50776992541f3983a821101d5209
QuentinK <qkrempp@qkp.fr>
2026-03-04 00:06:50 +0100

    Added stones for ammunitions

--- sorcerer.gd
+++ sorcerer.gd
@@ -21,12 +21,15 @@ func _process(_delta: float) -> void:
 		if Input.is_action_just_pressed("fire_1"):
 			fireball.color_mod = Color(1, 0, 0)
 			ammunition[0] -= 1
+			print("Remaining red: " + str(ammunition[0]))
 		elif  Input.is_action_just_pressed("fire_2"):
 			fireball.color_mod = Color(0, 1, 0)
 			ammunition[1] -= 1
+			print("Remaining green: " + str(ammunition[1]))
 		elif Input.is_action_just_pressed("fire_3"):
 			fireball.color_mod = Color(0, 0, 1)
 			ammunition[2] -= 1
+			print("Remaining blue: " + str(ammunition[2]))
 		self.get_parent().add_child(fireball)
 		
 	if Input.is_action_pressed("move_left"):
@@ -41,19 +44,26 @@ func _process(_delta: float) -> void:
 		$AnimatedSprite2D.stop()
 
 func _physics_process(delta: float) -> void:
+	
 	velocity.x = 0
 	if Input.is_action_pressed("move_left"):
 		velocity.x = -1 * speed
 	elif Input.is_action_pressed("move_right"):
 		velocity.x = 1 * speed
 		
-		
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = -jump_impulse
 	
 	if not is_on_floor():
-		print(fall_acceleration)
-		print(delta)
 		velocity.y += fall_acceleration * delta
 
+	for index in range(get_slide_collision_count()):
+		var collider = get_slide_collision(index).get_collider()
+		
+		if collider == null:
+			continue
+		elif collider.is_in_group("gemme_group"):
+			ammunition[0] += 1
+			var gemme = collider
+			gemme.delete()
 	move_and_slide()

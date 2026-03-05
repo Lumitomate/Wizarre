5eb66fa51e6d9fcc874d180eef4627b650a094fe
Lumitomate <thomas.krempp@ensaama.net>
2026-03-05 13:00:47 +0100

    Sprites + Niveau

--- scripts/spawner_ennemy.gd
+++ scripts/spawner_ennemy.gd
@@ -7,6 +7,7 @@ var ennemy_scene: PackedScene = preload("res://scenes/ennemy_flying.tscn")
 
 func _ready() -> void:
 	$SpawnCooldown.start()
+	$AnimatedSprite2D.play("SpawnerIdle")
 
 
 func _on_body_entered(body: Node2D) -> void:
@@ -21,6 +22,12 @@ func _on_body_exited(body: Node2D) -> void:
 
 func _on_spawn_cooldown_timeout() -> void:
 	if players_in_range.is_empty():
+		$AnimatedSprite2D.play("SpawnerApparition")
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if $AnimatedSprite2D.animation==&"SpawnerApparition":
 		var ennemy = ennemy_scene.instantiate()
 		ennemy.position = position
 		get_parent().add_child(ennemy)
+		$AnimatedSprite2D.play("SpawnerIdle")

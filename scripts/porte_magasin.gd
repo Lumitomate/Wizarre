3ab24ff6f8be570f2bb50a04ee2b355b20c05a3b
Lumitomate <thomas.krempp@ensaama.net>
2026-03-06 15:41:59 +0100

    Portes magasin epreuve

--- scripts/porte_magasin.gd
+++ scripts/porte_magasin.gd
@@ -4,13 +4,25 @@ extends StaticBody2D
 
 var can_open : bool = false
 
+var animation_suffix
+ 
 func _ready() -> void:
+	
+	match door_type:
+		Enum.DoorType.TIME:
+			animation_suffix="temps"
+		Enum.DoorType.NO_DAMAGE:
+			animation_suffix="dieu"
+		Enum.DoorType.FRIENDSHIP:
+			animation_suffix="amitie"
+		
+	$AnimatedSprite2D.animation="ouverture_" + animation_suffix
 	check_door_opening_condition()
 	$Timer.start()
 
 func open_door() -> void :
 	if can_open:
-		$AnimatedSprite2D.play("Ouverture")
+		$AnimatedSprite2D.play("ouverture_" + animation_suffix)
 
 func _process(_delta: float) -> void:
 	if $AnimatedSprite2D.frame == 10:
@@ -27,11 +39,11 @@ func check_door_opening_condition() -> void:
 		Enum.DoorType.FRIENDSHIP:
 			can_open = true
 			for k in GlobalInfo.run_info["players_info"].keys():
-				if k != "default" and GlobalInfo.run_info["players_info"][k]["lives"] == 0:
+				if str(k) != "default" and GlobalInfo.run_info["players_info"][k]["lives"] == 0:
 					can_open = false
 		Enum.DoorType.NO_DAMAGE:
 			for k in GlobalInfo.run_info["players_info"].keys():
-				if k != "default" and GlobalInfo.run_info["players_info"][k]["lives"] != 3:
+				if str(k) != "default" and GlobalInfo.run_info["players_info"][k]["lives"] != 3:
 					can_open = false
 			can_open = true
 	
